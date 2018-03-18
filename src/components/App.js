b240e40c283c2d68b099d99449db9399e4983356
Mizuki Zenta <mizukisb@gmail.com>
2018-03-19 10:03:29 +1100

    add leaderboard ui

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux"
 import { handleInitialData } from "../actions/share"
 import Dashboard from "./Dashboard"
 import LoadingBar from "react-redux-loading"
+import Leaderboard from "./Leaderboard"
 
 class App extends Component {
   componentDidMount() {
@@ -12,7 +13,7 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading ? null : <Dashboard />}
+        {this.props.loading ? null : <Leaderboard />}
       </div>
     )
   }
