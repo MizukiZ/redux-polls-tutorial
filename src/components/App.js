7d82b6dcacbcedafb0179d9f05030b537e86da03
Mizuki Zenta <mizukisb@gmail.com>
2018-03-18 20:53:13 +1100

    create dashboard ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { handleInitialData } from "../actions/share"
+import Dashboard from "./Dashboard"
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
-    return (
-      <div>
-        Starter Code.
-      </div>
-    )
+    return <div>{this.props.loading ? "loading" : <Dashboard />}</div>
   }
 }
 
-export default App
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
